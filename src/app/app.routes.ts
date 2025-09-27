585f964394d35b3d21d6e9abb5d4e78dcfe11eb6
Andrei Nicolae <andreicalutiu@gmail.com>
2025-09-27 18:28:37 +0300

    added guards to protect the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { Dashboard } from './features/dashboard/dashboard';
 import { Events } from './features/events/events';
 import { Templates } from './features/templates/templates';
 import { Auth } from './features/auth/auth';
+import { AuthGuard } from './core/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'auth', component: Auth },
-  { path: 'dashboard', component: Dashboard },
-  { path: 'events', component: Events },
-  { path: 'templates', component: Templates },
+  { path: 'dashboard', component: Dashboard, canActivate: [AuthGuard] },
+  { path: 'events', component: Events, canActivate: [AuthGuard] },
+  { path: 'templates', component: Templates, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '' }
 ];
